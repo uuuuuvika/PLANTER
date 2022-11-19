9fa4967d1ccd0182d318e103489290dc762ff875
Vika Shcherbak <vikashcherbaka@gmail.com>
2022-11-20 00:23:08 +0100

    style added, SIGNUP and LI

--- models/User.model.js
+++ models/User.model.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
     username: {
@@ -22,11 +21,9 @@ const userSchema = new Schema(
     },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
 );
 
 const User = model("User", userSchema);
-
 module.exports = User;
