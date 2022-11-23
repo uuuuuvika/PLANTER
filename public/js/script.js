c74cb899119da5333e1c940bf4db105bee2a8ae3
Vika Shcherbak <vikashcherbaka@gmail.com>
2022-11-23 23:56:11 +0100

    water done

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,15 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
 document.addEventListener("DOMContentLoaded", () => {
   console.log("PLANTER JS imported successfully!");
+  waterF()
 });
 
 function myFunction() {
-  var popup = document.getElementById("myPopup");
+  const popup = document.getElementById("myPopup");
   popup.classList.add("show");
+}
+function waterF(){
+  const water = document.querySelectorAll(".water");
+  const waterBool = water.innerHTML;
+  console.log(water.innerHTML)
 }
