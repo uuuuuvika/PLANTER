46028f1df0f8749a94e91203bd1409a270af5b50
Carolin Rieche <Ccarolin.rieche@yahoo.de>
2022-11-24 15:41:47 +0100

    map fix

--- app.js
+++ app.js
@@ -40,4 +40,9 @@ app.use("/auth", authRoutes);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
+app.use((req, res, next) => {
+    res.locals.currentUser = req.session.currentUser
+    next()
+})
+
 module.exports = app;
